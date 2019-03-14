a2a8cc5c24a0d5a2709e6b806a2d1ebcf7197f35
adanvetusta <adanvetusta@gmail.com>
2019-03-14 10:15:42 +0100

    Validadores
    Hecho validador de signUp.
    Faltan los de añadir usuarios y productos.

--- MyWallapop/src/main/java/com/uniovi/controllers/UserController.java
+++ MyWallapop/src/main/java/com/uniovi/controllers/UserController.java
@@ -7,12 +7,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
 import com.uniovi.services.ProductsService;
 import com.uniovi.services.RolesService;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -21,14 +25,16 @@ public class UserController {
 
 	@Autowired
 	private UsersService usersService;
-	
 
 	@Autowired
 	private SecurityService securityService;
 
 	@Autowired
 	private RolesService rolesService;
-	
+
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
+
 	@RequestMapping("/user/list")
 	public String getListado(Model model, Principal principal) {
 		User user = usersService.getUserByEmail(principal.getName());
@@ -55,10 +61,10 @@ public class UserController {
 		return "user/details";
 	}
 
-	@RequestMapping(value="/user/delete", method=RequestMethod.POST)
-	public String delete(@RequestParam(name="checkbox_ids") List<Long> ids, Principal principal) {
+	@RequestMapping(value = "/user/delete", method = RequestMethod.POST)
+	public String delete(@RequestParam(name = "checkbox_ids") List<Long> ids, Principal principal) {
 		User user = usersService.getUserByEmail(principal.getName());
-		usersService.deleteUser((ArrayList<Long>)ids, user);
+		usersService.deleteUser((ArrayList<Long>) ids, user);
 		return "redirect:/user/list";
 	}
 
@@ -77,12 +83,17 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signup() {
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
 		return "signup";
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute("user") User user, Model model) {
+	public String signup(@Validated @ModelAttribute("user") User user, Model model, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
 		user.setRole(rolesService.getRoles()[0]);
 		usersService.addUser(user);
 		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
@@ -100,5 +111,5 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "home";
 	}
-	
+
 }
